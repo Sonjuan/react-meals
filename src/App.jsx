213f0fe441357cabb73d554d14dc124a57fbdaee
Sonjuan <simset1212@gmail.com>
2023-02-27 15:17:27 +0900

    importing datafetching feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
+import Search from './components/Search'
+import Meals from './components/Meals'
+import Modal from './components/Modal'
+import Favorites from './components/Favorites'
 
 function App() {
   return (
     <main>
-        Meals App
+      {/* <Search /> */}
+      {/* <Favorites /> */}
+      <Meals />
+      {/* <Modal /> */}
     </main>
   )
 }
