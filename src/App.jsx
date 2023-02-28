46d49550b6322b70472de24048215cb182b7c611
Sonjuan <simset1212@gmail.com>
2023-02-28 12:13:57 +0900

    arrange css component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import './App.css';
-import Search from './components/Search'
+// import Search from './components/Search'
 import Meals from './components/Meals'
-import Modal from './components/Modal'
-import Favorites from './components/Favorites'
+// import Modal from './components/Modal'
+// import Favorites from './components/Favorites'
 
 function App() {
   return (
     <main>
-      {/* <Search /> */}
-      {/* <Favorites /> */}
       <Meals />
-      {/* <Modal /> */}
     </main>
   )
 }
--- src/context.jsx
+++ src/context.jsx
@@ -1,27 +1,32 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+
+import axios from 'axios'
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  const [meals, setMeals] = useState([])
+  const fetchMeals = async (url) => {
+    try {
+      const { data } = await axios.get(url)
+      setMeals(data.meals)
+    }
+    catch (e) {
+      console.log(e.response)
+    }
+  }
 
-    useEffect( () => {
-        const fetchData = async() => {
-            try {
-                const response = await fetch('https://randomuser.me/api/')
-                const data = await response.json()
-                console.log(data)
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        fetchData()
-    }, [])
+  useEffect(() => {
+      fetchMeals(allMealsUrl)
+  }, [])
 
-    return (
-        <AppContext.Provider value={{name: 'John', role:'student'}}>
-            {children}
-        </AppContext.Provider>
-    )
+  return (
+    <AppContext.Provider value={{ meals }}>
+      {children}
+    </AppContext.Provider>
+  )
 }
 
 export const useGlobalContext = () => {
